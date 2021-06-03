26746f25db5e91274a98f1d2520673ba175761b7
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-03 18:38:37 -0300

    Initial Commit

---
+++ FastFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastFood
+//
+//  Created by Raphael Cerqueira on 03/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastFood/FastFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastFoodApp.swift
+//  FastFood
+//
+//  Created by Raphael Cerqueira on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FastFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
